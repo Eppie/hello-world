4f7b6c04dfc7119488e8d0c222a04b8f6db0ba16
Andrew Epstein <aepstein@udel.edu>
2016-06-30 16:32:48 -0400

    Added fizzbuzz in javascript

---
+++ fizzbuzz/node/fizzbuzz.js
@@ -0,0 +1,17 @@
+function fizzbuzz(n) {
+	for (var i = 1; i <= n; i++) {
+		var output = "";
+
+		if (i % 3 == 0) {
+			output += "Fizz";
+		}
+
+		if (i % 5 == 0) {
+			output += "Buzz";
+		}
+
+		console.log(output || i);
+	}
+}
+
+fizzbuzz(100);
